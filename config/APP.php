d0b7156a9da474dc367abcb299ee9ace3f7d27e9
Jhonny <jhonnydanieldavila@gmail.com>
2026-01-12 12:39:12 -0400

    qlq

---
+++ config/APP.php
@@ -0,0 +1,4 @@
+<?php
+    const APP_URL = "http://localhost/PROYECTO_TRAYECTO_II/";
+    const APP_NAME = "PROYECTO TRAYECTO II";
+    date_default_timezone_set('America/Caracas');
---
+++ config/SERVER.php
@@ -0,0 +1,13 @@
+<?php
+	//--------------------Base de Datos---------------------------
+    const SERVER = "localhost";
+    const DB = "PROYECTO_TRAYECTO_II";
+    const USER = "root";
+    const PASS = "";
+
+    const SGBD = "mysql:host=".SERVER.";dbname=".DB;
+
+    const METHOD = "AES-256-CBC";
+    const SECRET_KEY = '$PROYECTO_TRAYECTO_II@2025';
+    const SECRET_IV = '012345';
+    //-------------------------------------------------------------
